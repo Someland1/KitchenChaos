66d3f0d782c38f2cc47ebb3a974424d77cf481e0
Tonny Lee <44522925+Someland1@users.noreply.github.com>
2023-07-30 21:18:55 +0900

    add collider test
    add collider test

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -17,7 +17,48 @@ public class Player : MonoBehaviour
 
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
 
-        transform.position += moveDir * Time.deltaTime * moveSpeed;
+        float moveDistance = moveSpeed * Time.deltaTime;
+        float playerRadius = 0.7f;
+        float playerHeight = 2;
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position +Vector3.up * playerHeight, playerRadius, moveDir, moveDistance);
+        if(!canMove)
+        {
+            // Cannot move towards moveDir
+
+            // Attempt only X movement
+            Vector3 moveDirX = new Vector3(moveDir.x, 0, 0);
+            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
+
+            if(canMove)
+            {
+                //Can move only on the X axis
+                moveDir = moveDirX;
+            }
+            else
+            {
+                // Cannot move only on the X
+
+                // Attempt only Z movement
+                Vector3 moveDirZ = new Vector3(0, 0, moveDir.z);
+                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance);
+                
+                if (canMove)
+                {
+                    // Can move only on the Z
+                    moveDir = moveDirZ;
+                }
+                else
+                {
+                    // Cannot move in any direction.
+                }
+            }
+        }
+
+        
+        if (canMove)
+        {
+            transform.position += moveDir * moveDistance;
+        }
 
         isWalking = moveDir != Vector3.zero;
 
